82c58379cbd49b1a0f654f004af3cb29699db86f
Neet <2370875301@qq.com>
2022-04-04 22:03:22 +0800

    20220404

--- app/src/main/java/com/zhu/keshee/MainActivity.kt
+++ app/src/main/java/com/zhu/keshee/MainActivity.kt
@@ -3,6 +3,7 @@ package com.zhu.keshee
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.core.view.GravityCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -15,11 +16,19 @@ class MainActivity : AppCompatActivity() {
             it.setDisplayHomeAsUpEnabled(true)
             it.setHomeAsUpIndicator(R.drawable.ic_menu)
         }//增加按键调动侧边账户栏
+        navView.setCheckedItem(R.id.login)
+        navView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.login->Toast.makeText(this,"ss",Toast.LENGTH_SHORT).show()
+                R.id.regist->Toast.makeText(this,"ss",Toast.LENGTH_SHORT).show()
+            }
+             true
+        }//侧边栏按钮点击事件注册
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item.itemId){
-            android.R.id.home ->drawerLayout.openDrawer(GravityCompat.START)}
+            android.R.id.home ->drawerLayout.openDrawer(GravityCompat.START)}//设置bar栏的呼出侧边按钮
         return true
     }
 }
